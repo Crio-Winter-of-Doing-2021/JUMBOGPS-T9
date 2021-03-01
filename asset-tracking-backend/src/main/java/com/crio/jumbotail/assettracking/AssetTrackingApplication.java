c1f581bc29c0b9b91ca932e1b93618aeb904e52c
anugrahsinghal <anugrahsinghal1@gmail.com>
2021-03-01 16:39:55 +0530

    initial commit for backend

---
+++ asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/AssetTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.crio.jumbotail.assettracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetTrackingApplication.class, args);
+	}
+
+}
