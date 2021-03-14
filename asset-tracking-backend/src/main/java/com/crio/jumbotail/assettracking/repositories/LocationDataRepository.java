c0875eef198574333c07d0cd09e7cbd88469c962
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-03-15 01:19:05 +0530

    refactor: remove unused code, renamed function params

--- asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/repositories/LocationDataRepository.java
+++ asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/repositories/LocationDataRepository.java
@@ -7,34 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationDataRepository extends JpaRepository<LocationData, Long> {
 
-	List<LocationData> findAllByAsset_IdAndTimestampBetween(Long assetId, LocalDateTime startTime, LocalDateTime endTime);
-	List<LocationData> findAllByAsset_IdAndTimestampBetweenOrderByTimestampDesc(Long assetId, LocalDateTime startTime, LocalDateTime endTime);
-
-
-
-	//	@Query(value = "select location from LocationData location where location.asset.id = ?1 AND current_timestamp > (Select loc.timestamp from LocationData loc where loc.asset.id = ?1)")
-//	List<LocationData> find24HourData(Long assetId);
-
-	@Query(value = "select max (location.timestamp) from LocationData location where location.asset.id = ?1")
-	LocalDateTime findMax(Long assetId);
-
-	@Query(value = "select location from LocationData location where location.asset.id = ?1 "
-//	               + "AND max (timestamp)")
-	               + "AND ?2 > (select max (location.timestamp) from LocationData location where location.asset.id = ?1)")
-	LocalDateTime find24Hr(Long assetId, LocalDateTime localDateTime);
-
-
-	@Query(value = "SELECT location from LocationData location where location.asset.id = ?1 ORDER BY location.timestamp DESC")
-	Page<LocationData> findLastNLocationsOfAnAsset(Long assetId, Pageable pageable);
+	List<LocationData> findAllByAsset_IdAndTimestampBetweenOrderByTimestampDesc(Long assetId, LocalDateTime twentyFourHourBeforeTimestamp, LocalDateTime currentTime);
 
 	Page<LocationData> findByAsset_IdOrderByTimestampDesc(Long assetId, Pageable pageable);
-
 
 	default Page<LocationData> findLastLocationsOfAnAsset(Long assetId) {
 		Pageable singleRecord = PageRequest.of(0, 1);
