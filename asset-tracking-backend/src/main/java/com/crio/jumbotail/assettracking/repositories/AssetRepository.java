4d9d44260861fd55b89681f76a531805c431b01d
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-03-17 20:33:39 +0530

    some fixes

--- asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/repositories/AssetRepository.java
+++ asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/repositories/AssetRepository.java
@@ -3,6 +3,7 @@ package com.crio.jumbotail.assettracking.repositories;
 import com.crio.jumbotail.assettracking.entity.Asset;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,18 +11,57 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
-	// last (100 by default) assets
-	List<Asset> findAllByOrderByLastReportedTimestampDesc(Pageable pageable);
+	// last N assets (N = 100 by default)
+	List<Asset> findAllByOrderByLastReportedTimestampDesc(
+			Pageable pageable
+	);
 
 
 	// asset type filter
-	List<Asset> findAllByAssetTypeOrderByLastReportedTimestampDesc(String assetType, Pageable pageable);
+	List<Asset> findAllByAssetTypeOrderByLastReportedTimestampDesc(
+			String assetType, Pageable pageable
+	);
 
 
 	// time based filter for all assets
-	List<Asset> findAllByLastReportedTimestampBetween(LocalDateTime startTime, LocalDateTime endTime, Pageable pageable);
+	List<Asset> findAllByLastReportedTimestampBetween(
+			LocalDateTime startTime, LocalDateTime endTime,
+			Pageable pageable
+	);
+
+	// time + type filter
+	List<Asset> findAllByAssetTypeAndLastReportedTimestampBetween(
+			String assetType,
+			LocalDateTime startTime, LocalDateTime endTime,
+			Pageable pageable
+	);
+
+	List<Asset> findAllByAssetTypeInOrderByLastReportedTimestampDesc(
+			List<String> assetTypes, Pageable pageable
+	);
+
+	List<Asset> findAllByAssetTypeInAndLastReportedTimestampBetween(
+			List<String> assetTypes,
+			LocalDateTime startTime, LocalDateTime endTime,
+			Pageable pageable
+	);
+
+	default List<Asset> filterAssetsByType(String assetType, Pageable pageable) {
+		return findAllByAssetTypeOrderByLastReportedTimestampDesc(assetType, pageable);
+	}
+
+	default List<Asset> filterAssetsByTypeAndTime(String assetType,
+	                                              LocalDateTime startTime, LocalDateTime endTime,
+	                                              Pageable pageable) {
+		return findAllByAssetTypeAndLastReportedTimestampBetween(assetType, startTime, endTime, pageable);
+	}
+
+	default List<Asset> findAssets(Pageable pageable) {
+		return findAllByOrderByLastReportedTimestampDesc(pageable);
+	}
 
 
-
-
+	default List<Asset> filterAssetsByTime(LocalDateTime startDateTime, LocalDateTime endDateTime, PageRequest pageReqWithLimit) {
+		return findAllByLastReportedTimestampBetween(startDateTime, endDateTime, pageReqWithLimit);
+	}
 }
--- asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/service/AssetDataRetrievalServiceImpl.java
+++ asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/service/AssetDataRetrievalServiceImpl.java
@@ -6,12 +6,13 @@ import static java.time.temporal.ChronoUnit.HOURS;
 
 import com.crio.jumbotail.assettracking.entity.Asset;
 import com.crio.jumbotail.assettracking.entity.LocationData;
+import com.crio.jumbotail.assettracking.exceptions.AssetNotFoundException;
 import com.crio.jumbotail.assettracking.repositories.AssetRepository;
 import com.crio.jumbotail.assettracking.repositories.LocationDataRepository;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Optional;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,12 +56,10 @@ public class AssetDataRetrievalServiceImpl implements AssetDataRetrievalService 
 	@Override
 	public List<LocationData> getHistoryForAsset(Long assetId) {
 
-		final Instant instant = Instant.now();
-		final LocalDateTime currentTimestamp = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
-		final LocalDateTime twentyFourHourBeforeTimestamp = LocalDateTime.ofInstant(instant, ZoneOffset.UTC).minus(24, HOURS);
-
-
-		LOG.info("Checking for data between {} and {}", twentyFourHourBeforeTimestamp, currentTimestamp);
+		final boolean assetExists = assetRepository.existsById(assetId);
+		if (!assetExists) {
+			throw new AssetNotFoundException("Asset not found for Id - " + assetId);
+		}
 
 		final List<LocationData> last24HourHistory = locationDataRepository.findAllByAsset_IdAndTimestampBetweenOrderByTimestampDesc(assetId,
 				LocalDateTime.now().minus(24, HOURS),
@@ -69,6 +68,88 @@ public class AssetDataRetrievalServiceImpl implements AssetDataRetrievalService 
 		LOG.info("last24HourHistory [{}]", last24HourHistory);
 
 		return last24HourHistory;
+	}
+
+	@Override
+	public Asset getAssetForId(Long assetId) {
+		final Optional<Asset> asset = assetRepository.findById(assetId);
+
+		return asset.orElseThrow(() -> new AssetNotFoundException("Asset not found for Id - " + assetId));
+	}
+
+
+	// TODO : filters
+	// (all will support limits + and will be sorted based on timestamp)
+	// TODO 1. Filter on (start-time and end-time)
+	// add validation
+	// query - done
+	// TODO 2. Filter on (start-time and end-time) + type
+	// add validation
+	// query - done
+	// TODO 3. Filter on type
+	// add validation
+	// query - done
+	@Override
+	public List<Asset> getAssetFilteredBy(String assetType, Long startTimestamp, Long endTimestamp, int limit) {
+		List<Asset> assets = null;
+
+		final PageRequest pageRequest = PageRequest.of(0, limit);
+		if (hasNoFiltersDefined(assetType, startTimestamp, endTimestamp)) {
+			LOG.info("No filters defined. Getting all assets.");
+			assets = assetRepository.findAssets(pageRequest);
+		} else if (hasOnlyTypeFilterDefined(assetType, startTimestamp, endTimestamp)) {
+			LOG.info("Type filter defined");
+			assets = assetRepository.filterAssetsByType(assetType, pageRequest);
+		} else if (hasOnlyTimeFilterDefined(assetType, startTimestamp, endTimestamp)) {
+			throwIfInvalidTimeFilter(startTimestamp, endTimestamp);
+			final LocalDateTime startDateTime = localDateTimeFromTimestamp(startTimestamp);
+			final LocalDateTime endDateTime = localDateTimeFromTimestamp(endTimestamp);
+			assets = assetRepository.filterAssetsByTime(startDateTime, endDateTime, pageRequest);
+		} else if (hasBothTimeAndTypeFilter(assetType, startTimestamp, endTimestamp)) {
+			throwIfInvalidTimeFilter(startTimestamp, endTimestamp);
+			final LocalDateTime startDateTime = localDateTimeFromTimestamp(startTimestamp);
+			final LocalDateTime endDateTime = localDateTimeFromTimestamp(endTimestamp);
+			assets = assetRepository.filterAssetsByTypeAndTime(assetType, startDateTime, endDateTime, pageRequest);
+		}
+
+		return assets;
+	}
+
+	private void throwIfInvalidTimeFilter(Long startTimestamp, Long endTimestamp) {
+		if (!isValidTimeFilter(startTimestamp, endTimestamp)) {
+			throw new IllegalArgumentException("Start time should not be less than end date time");
+		}
+	}
+
+	private boolean hasBothTimeAndTypeFilter(String assetType, Long startTimestamp, Long endTimestamp) {
+		return !StringUtils.isEmpty(assetType) && (startTimestamp != null && endTimestamp != null);
+	}
+
+	private boolean hasOnlyTimeFilterDefined(String assetType, Long startTimestamp, Long endTimestamp) {
+		return StringUtils.isEmpty(assetType) && (startTimestamp != null && endTimestamp != null);
+	}
+
+	private boolean hasNoFiltersDefined(String assetType, Long startTimestamp, Long endTimestamp) {
+		return StringUtils.isEmpty(assetType) && (startTimestamp == null || endTimestamp == null);
+	}
+
+	private boolean hasOnlyTypeFilterDefined(String assetType, Long startTimestamp, Long endTimestamp) {
+		return !StringUtils.isEmpty(assetType) && (startTimestamp == null || endTimestamp == null);
+	}
+
+	private boolean isValidTimeFilter(Long startTimestamp, Long endTimestamp) {
+		// start say 1 - Jan - 2020
+		// end say 20 - Jan - 2020
+		// is a valid combination
+		final boolean isValid = startTimestamp <= endTimestamp;
+
+		LOG.info("startDateTime [{}] , endDateTime [{}], isValid [{}]", startTimestamp, endTimestamp, isValid);
+
+		return isValid;
+	}
+
+	private LocalDateTime localDateTimeFromTimestamp(Long timestamp) {
+		return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), systemDefault());
 	}
 
 }
