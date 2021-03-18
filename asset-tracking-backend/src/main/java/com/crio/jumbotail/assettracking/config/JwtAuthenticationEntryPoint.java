d563f78cfa799354d0f3d20965d4e138ac43092b
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-03-19 00:37:34 +0530

    refactor: minor code refactor

--- asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/config/JwtAuthenticationEntryPoint.java
+++ asset-tracking-backend/src/main/java/com/crio/jumbotail/assettracking/config/JwtAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -13,6 +14,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
@@ -24,26 +28,27 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 		Exception exception = (Exception) request.getAttribute("exception");
 
 		String message;
-
 		byte[] body;
+
 		if (exception != null) {
-
-			body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("cause", exception.toString()));
-
+			body = objectMapper.writeValueAsBytes(Collections.singletonMap("cause", exception.toString()));
 			response.getOutputStream().write(body);
-
 		} else {
-
-			if (authException.getCause() != null) {
-				message = authException.getCause().toString() + " " + authException.getMessage();
-			} else {
-				message = authException.getMessage();
-			}
-
-			body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("error", message));
-
+			message = getExceptionMessage(authException);
+			body = objectMapper.writeValueAsBytes(Collections.singletonMap("error", message));
 		}
+
 		response.getOutputStream().write(body);
+	}
+
+	private String getExceptionMessage(AuthenticationException authException) {
+		String message;
+		if (authException.getCause() != null) {
+			message = authException.getCause().toString() + " " + authException.getMessage();
+		} else {
+			message = authException.getMessage();
+		}
+		return message;
 	}
 
 }
